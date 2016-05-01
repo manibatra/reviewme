17c811ca89aaa9029d9184631a35bc3aa71d3795
Mani Batra <manibatra2002@gmail.com>
2016-05-01 12:35:20 +0530

    password reset flow added

--- reviewme/reviewme/urls.py
+++ reviewme/reviewme/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views as global_views
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
@@ -23,4 +25,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/', global_views.login_view, name='login'),
     url(r'^users/', include('users.urls', namespace="users")),
+    url(r'^users/reset-password-done/$', auth_views.password_reset_done, {'template_name': 'users/password_reset_done.html'}, name="password_reset_done"),
+     url(r'^password/reset-password-confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm, {'template_name': 'users/password_reset_confirm.html'}, name="password_reset_confirm"),
+    url(r'^password/reset-password-complete/$', auth_views.password_reset_complete, {'template_name': 'users/password_reset_complete.html'}, name="password_reset_complete"),
 ]
--- reviewme/users/urls.py
+++ reviewme/users/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -10,5 +11,9 @@ urlpatterns = [
 	url(r'^verification-confirm/(?P<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})(?P<ver_code>[0-9a-zA-Z]{32})/$'
 					, views.verification_confirm, name='verification_confirm'),
 	url(r'^verification-complete/$', views.verification_complete, name='verification_complete'),
+	url(r'^reset-password/$', auth_views.password_reset, {'template_name': 'users/password_reset_start.html'}, name='password_reset_start'),
+	url(r'^reset-password-done/$', auth_views.password_reset_done, {'template_name': 'users/password_reset_done.html'}, name="password_reset_done"),
+
+
 
 ]
