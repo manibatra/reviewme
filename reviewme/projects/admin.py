5cc18053278096d8e81d0f605dc4c7083b3bcbbe
Mani Batra <manibatra2002@gmail.com>
2016-05-07 15:45:49 +0530

    registerd models to admin

--- reviewme/projects/admin.py
+++ reviewme/projects/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from  .models import Role
+from  .models import Role, Category, SubCategory, Project, Review
 
 # Register your models here.
 
 admin.site.register(Role)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Project)
+admin.site.register(Review)
+
