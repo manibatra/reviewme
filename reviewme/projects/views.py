7b945c6fe2b9f746abe566ca4620270b246012c0
Mani Batra <manibatra2002@gmail.com>
2016-05-08 19:40:41 +0530

    displaying project categories

--- reviewme/projects/views.py
+++ reviewme/projects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Category
+from django.db.models import Count
 
 # Create your views here.
 def showCategories(request):
-	return render(request, 'projects/categories.html')
+	context = {}
+	all_categories = Category.objects.annotate(num_projects=Count('subcategory__project', distinct=True))
+	context['items'] = all_categories
+	return render(request, 'projects/categories.html', context)
