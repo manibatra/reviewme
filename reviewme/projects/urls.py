773a936b3bb3954b9026dcf43310a7fdbe68b9ea
Mani Batra <manibatra2002@gmail.com>
2016-05-08 19:53:20 +0530

    displaying dynamic subcategories

--- reviewme/projects/urls.py
+++ reviewme/projects/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^categories/$', views.showCategories, name='categories'),
+	url(r'^(?P<category_id>[0-9]+)/$', views.showSubCategories, name='sub_categories'),
+
 ]
--- reviewme/projects/views.py
+++ reviewme/projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, SubCategory
 from django.db.models import Count
 
 # Create your views here.
@@ -7,4 +7,10 @@ def showCategories(request):
 	context = {}
 	all_categories = Category.objects.annotate(num_projects=Count('subcategory__project', distinct=True))
 	context['items'] = all_categories
+	return render(request, 'projects/categories.html', context)
+
+def showSubCategories(request, category_id):
+	context = {}
+	all_sub_categories = SubCategory.objects.filter(category=category_id).annotate(num_projects=Count('project', distinct=True))
+	context['items'] = all_sub_categories
 	return render(request, 'projects/categories.html', context)
