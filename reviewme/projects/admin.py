980d67cb3cb96fce8b122d2b638df72733dd6735
Mani Batra <manibatra2002@gmail.com>
2016-05-10 20:53:33 +0530

    unregisterd the group model from admin

--- reviewme/projects/admin.py
+++ reviewme/projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from  .models import Role, Category, SubCategory, Project, Submission
+from  .models import Role, Category, SubCategory, Project, Submission, Reviewer
 
 # Register your models here.
 
@@ -8,4 +8,5 @@ admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Project)
 admin.site.register(Submission)
+admin.site.register(Reviewer)
 
