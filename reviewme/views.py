0756f4f7a2d2a579047d289d2d6fc3d524493b26
Mani Batra <manibatra2002@gmail.com>
2016-05-26 03:10:33 +0530

    returning plain text

--- reviewme/views.py
+++ reviewme/views.py
@@ -8,4 +8,4 @@ def custom_500_view(request):
 	return render(request, "message.html", {'heading' : 'HTTP 500 ERROR', 'message' : 'Oops! Something broke at our end! We are "Reviewing" it ASAP!!!'})
 
 def letsencrypt(request, id):
-	return HttpResponse("ey75qXa81d3mMXDqG7oHH2zR-U7je4JDGvnSZxNYfqw.aG1MGpsjuniwg0DZThLFdS7HIWqEwsAGBXppIBQBLNQ")
+	return HttpResponse("ey75qXa81d3mMXDqG7oHH2zR-U7je4JDGvnSZxNYfqw.aG1MGpsjuniwg0DZThLFdS7HIWqEwsAGBXppIBQBLNQ", content_type='text/plain')
