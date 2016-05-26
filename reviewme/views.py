1e4f8e556a35bda4f319ca0e79db46815ce68450
Mani Batra <manibatra2002@gmail.com>
2016-05-27 01:58:42 +0530

    letsencrypt fixes

--- reviewme/views.py
+++ reviewme/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 def custom_404_view(request):
 	return render(request, "message.html", {'heading' : 'HTTP 404 ERROR', 'message' : 'Oops! The page you are trying to view does not exist!'})
@@ -8,4 +9,4 @@ def custom_500_view(request):
 	return render(request, "message.html", {'heading' : 'HTTP 500 ERROR', 'message' : 'Oops! Something broke at our end! We are "Reviewing" it ASAP!!!'})
 
 def letsencrypt(request, id):
-	return HttpResponse("ey75qXa81d3mMXDqG7oHH2zR-U7je4JDGvnSZxNYfqw.aG1MGpsjuniwg0DZThLFdS7HIWqEwsAGBXppIBQBLNQ", content_type='text/plain')
+	return HttpResponse(settings.LENCRYPT_KEY, content_type='text/plain')
