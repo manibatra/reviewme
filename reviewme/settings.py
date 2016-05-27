0baec2e770961b2e7e20aba2fe6213023c706c77
Mani Batra <manibatra2002@gmail.com>
2016-05-27 06:51:48 +0530

    original settings restored

--- reviewme/settings.py
+++ reviewme/settings.py
@@ -36,8 +36,8 @@ except:
     pass
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
