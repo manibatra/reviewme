b6d39a711c59e1be691b33a687341e78b1db3b81
Mani Batra <manibatra2002@gmail.com>
2016-06-04 03:23:43 +0530

    completed demo initial version

--- demo/views.py
+++ demo/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from projects.models import Project, Objective, Resource
+from django.conf import settings
 
 # Create your views here.
 def begin(request):
 	context = {}
+	context['media'] = settings.MEDIA_URL
 	current_project = Project.objects.get(pk=1)
 	context['project'] = current_project
 	context['status'] = "No"
