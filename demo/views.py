fca5af1116dd26959a44b97574583454f6e7012b
Mani Batra <manibatra2002@gmail.com>
2016-06-04 01:26:56 +0530

    rendering first step

--- demo/views.py
+++ demo/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def begin(request):
 	context = {}
+	current_project = Project.objects.get(pk=1)
+	context['project'] = current_project
+	context['status'] = "No"
+	context['objectives'] = Objective.objects.filter(project=current_project)
+	context['resources'] = Resource.objects.filter(project=current_project).order_by('id')
 	return render(request, 'demo/step1.html', context)
 
 def middle(request):
