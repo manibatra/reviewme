cae21bdfa2bff75c1fd39717b947211e1b87a2ba
Mani Batra <manibatra2002@gmail.com>
2016-06-07 05:15:56 +0530

    created urls

--- demo/urls.py
+++ demo/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
 
 	url(r'^begin/$', views.begin, name='begin'),
+	url(r'^intro/start/$', views.start_intro, name='start_intro'),
+	url(r'^intro/submit/$', views.submit_intro, name='submit_intro'),
+	url(r'^intro/view/$', views.view_intro, name='view_intro'),
 
 ]
